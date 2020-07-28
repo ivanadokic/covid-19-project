37e42106034ecbe459d70c97632fd38d4b5b994e
Ivana <ivana_pile@yahoo.com>
2020-07-27 20:16:34 -0400

    cleaning code

--- covid-19-api/app/controllers/countries_controller.rb
+++ covid-19-api/app/controllers/countries_controller.rb
@@ -1,34 +1,12 @@
 class CountriesController < ApplicationController
-    def index
-        @countries = Country.all
-        render json: @countries.to_json(:except => [:updated_at, :created_at])
-    end
-
-    def show
-        @country = Country.find(params[:id])
-        render json: @country.to_json(:except => [:updated_at, :created_at])
-    end
-
-    def create
-        @country = Country.new(country_params)
-        if @country.save
-            render json: @country.to_json(:except => [:updated_at, :created_at])
-        else
-            render json: {error: "Country cannot be saved to the database. Please try again."}
-        end
-    end
-
-    def destroy
-        @country = Country.find(params[:id])
-        @country.destroy
-        render json: {alert: "Country Deleted"}
-    end
-
-    private
-
-    def country_params
-        params.require(:country).permit(:cases, :recovered, :deaths, :population )
-
-    end
-end
+  def index
+    @countries = Country.all
+    render json: @countries.to_json(:except => [:updated_at, :created_at])
+  end
+  
+  def show
+    @country = Country.find(params[:id])
+    render json: @country.to_json(:except => [:updated_at, :created_at])
+  end
+ 
 end
