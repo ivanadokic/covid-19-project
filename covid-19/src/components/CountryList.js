b02336f85d1b8750eaedd117a58d34443141080f
Ivana <ivana_pile@yahoo.com>
2020-08-12 15:43:04 -0400

    search country form

--- covid-19/src/components/CountryList.js
+++ covid-19/src/components/CountryList.js
@@ -1,23 +1,65 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-
 import Columned from 'react-columned';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { connect } from 'react-redux'
 import { fetchCountries } from '../actions/index'
 import CountryCard from './CountryCard'
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class CountryList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      search: ""
+    }
+  }
+
   componentDidMount() {
     this.props.fetchCountries()
   }
+  setSearchCountries = e => {
+    e.preventDefault()
+    this.setState({
+      search: e.target.children[0].children[1].value
+    })
+  }
 
+  handlechange = e => {
+    e.preventDefault
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
+  }
   render() {
-    const allcountries = this.props.countries.map((data, i) => <CountryCard data={data} key={i} />)
+
+    // const [searchCountries, setSearchCountries] = ("");
+
+    const filterCountry = this.props.countries.filter(item => item.country.toLowerCase().includes(this.state.search.toLowerCase()))
+    const allcountries = filterCountry.map((data, i) => <CountryCard data={data} key={i} />)
+
     return (
-      < Columned>{allcountries}</Columned >
+      <div>
+        < Form onSubmit={this.setSearchCountries}>
+          <Form.Group controlId="formGroupSearch">
+            <Form.Label>Search Country</Form.Label>
+            <Form.Control
+              type="text"
+              style={{ margin: "10px" }}
+              placeholder="Enter Country name"
+              name="search"
+            //onChange={this.handlechange}
+            //value={this.state.search}
+            />
+          </Form.Group>
+          <Button variant="success" type="submit" >
+            Search
+            </Button>
+        </Form >
+        < Columned>{allcountries}</Columned >
+      </div>
     )
   }
 }
